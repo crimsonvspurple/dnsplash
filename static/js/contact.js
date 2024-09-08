0572dea7dc659b42203c2a4df2d6ca869f230b7d
iamahad <mohammad.ahad@xen.works>
2024-09-08 18:21:49 +0600

    fixed ui issues

---
+++ static/js/contact.js
@@ -0,0 +1,43 @@
+function submitContactForm() {
+    event.preventDefault();
+    const form = document.getElementById('contactForm');
+    const emailInput = document.getElementById('contactEmail').value;
+    const subjectInput = document.getElementById('contactSubject').value;
+    const messageInput = document.getElementById('contactMessage').value;
+    if (emailInput && subjectInput && messageInput) { 
+        fetch("https://core.api.dobbersports.com/v1/feedback/contact", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"email": emailInput, "subject": subjectInput, "message": messageInput}),
+
+        })
+        .then(response => {
+            if (response.ok) {
+                showSnackbar('We\'ve got your message!');
+                form.reset();
+            }else if(response.status == 400){
+                showSnackbar('Something Wrong!');
+                throw new Error('Network response was not ok');
+            }
+        })
+    } else {
+        showSnackbar('Please enter a valid email');
+    }
+}
+
+function showSnackbar(message) {
+    if(message == null) message = "You have been subscribed successfully!";
+    var snackbar = document.getElementById("snackbar");
+    var messageSpan = document.getElementById("snackbar-message");
+    
+    messageSpan.textContent = message; // Set the message
+    snackbar.classList.remove("hidden");
+    snackbar.classList.add("show");
+
+    setTimeout(function() {
+        snackbar.classList.remove("show");
+        snackbar.classList.add("hidden");
+    }, 6000);
+}
---
+++ static/js/notify.js
@@ -0,0 +1,43 @@
+function submitForm() {
+    event.preventDefault();
+    const form = document.getElementById('notifyMe');
+    const emailInput = document.getElementById('cta-email').value;
+    // Validate email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput)) {
+        showSnackbar('Please enter a valid email');
+        return;
+    }
+    if (emailInput) { 
+        fetch("https://user.api.dobbersports.com/v1/user/subscribe/"+emailInput, {
+            method: 'POST',
+        })
+        .then(response => {
+            if (response.ok) {
+                showSnackbar('You have been subscribed successfully!');
+                form.reset();
+            }else if(response.status == 400){
+                showSnackbar('You have been subscribed successfully!');
+                form.reset();
+            }
+            throw new Error('Network response was not ok');
+        })
+    } else {
+        showSnackbar('Please enter a valid email');
+    }
+}
+
+function showSnackbar(message) {
+    if(message == null) message = "You have been subscribed successfully!";
+    var snackbar = document.getElementById("snackbar");
+    var messageSpan = document.getElementById("snackbar-message");
+    
+    messageSpan.textContent = message; // Set the message
+    snackbar.classList.remove("hidden");
+    snackbar.classList.add("show");
+
+    setTimeout(function() {
+        snackbar.classList.remove("show");
+        snackbar.classList.add("hidden");
+    }, 6000);
+}
